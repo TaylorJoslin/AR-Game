a84eee0beb985de9aa029fa5878d852e92e5eb24
TaylorJoslin <143643796+TaylorJoslin@users.noreply.github.com>
2023-11-15 13:18:35 -0500

    Added charged shot

--- Assets/02.0 AR Shooter/Scripts/ARShoot.cs
+++ Assets/02.0 AR Shooter/Scripts/ARShoot.cs
@@ -7,21 +7,25 @@ using UnityEngine.XR.Interaction.Toolkit.AR;
 
 public class ARShoot : MonoBehaviour
 {
-    public GameObject projectile;
+    public GameObject projectile,chargedProjectile;
     public float shotSpeed = 4f;
     public AudioClip shootClip;
-    public GameObject player;
+    public GameObject player,charageShot;
 
     public Camera main;
 
     ARGestureInteractor arGestureInteractor;
-    
 
+    private void Awake()
+    {
+        charageShot.SetActive(false);
+    }
     void OnEnable()
     {
         arGestureInteractor = GetComponent<ARGestureInteractor>();
         arGestureInteractor.tapGestureRecognizer.onGestureStarted += OnTapRecognized;
         arGestureInteractor.dragGestureRecognizer.onGestureStarted += OnDragRecognized;
+        
     }
 
 	void Disable()
@@ -34,12 +38,40 @@ public class ARShoot : MonoBehaviour
     {
         Debug.Log("DRAG START: " + obj.position);
         obj.onFinished += OnDragComplete;
+
+        charageShot.SetActive(true);
     }
 
 	private void OnDragComplete(DragGesture obj)
 	{
         Debug.Log("DRAG END" + obj.position);
         obj.onFinished -= OnDragComplete;
+
+        charageShot.SetActive(false);
+
+        // this conditional (from Vector2Extensions) avoids spawning when tapping a button
+        Vector2 mousePos = obj.startPosition;
+        if (!mousePos.IsPointOverUIObject())
+        {
+            // creating a projectile and setting its velocity
+
+            Ray ray = main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            Vector3 targetPoint;
+            if (Physics.Raycast(ray, out hit))
+                targetPoint = hit.point;
+            else
+                targetPoint = ray.GetPoint(75);
+
+            Vector3 directionWithoutSpread = targetPoint = transform.position;
+
+            GameObject newProjectile = Instantiate(chargedProjectile, transform.position, Quaternion.Euler(90f, 0f, 0f));
+            Vector3 shotVelocity = transform.forward.normalized * shotSpeed;
+            newProjectile.GetComponent<Rigidbody>().velocity = shotVelocity;
+
+
+            GetComponent<AudioSource>().PlayOneShot(shootClip);
+        }
     }
 
     public void ChangeProjectile(GameObject newPrefab)
@@ -48,6 +80,11 @@ public class ARShoot : MonoBehaviour
         projectile = newPrefab;        
     }
 
+    public void ChargedProjectile(GameObject newPrefab)
+    {
+        chargedProjectile = newPrefab;
+    }
+    
 
     private void OnTapRecognized(TapGesture obj)
     {
